fdc5794f0e495f5786ef1966709c3fc18989debc
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-05-08 20:14:50 +0600

    Creating Spring Boot application

---
+++ src/main/java/kz/zhelezyaka/springbootexperiments/SpringBootExperimentsApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.springbootexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExperimentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExperimentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/zhelezyaka/springbootexperiments/SpringBootExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.springbootexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExperimentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
