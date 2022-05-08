a057cd9ecd4b8549fece71bf8a92f41bd321f0dd
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-05-08 21:16:19 +0600

    Creating REST API. Part one

---
+++ src/main/java/kz/zhelezyaka/springbootexperiments/dao/EmployeeDAOImpl.java
@@ -0,0 +1,45 @@
+package kz.zhelezyaka.springbootexperiments.dao;
+
+import kz.zhelezyaka.springbootexperiments.entity.Employee;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        Session session = entityManager.unwrap(Session.class);
+        List<Employee> allEmployees = session.createQuery("from Employee", Employee.class)
+                .getResultList();
+        return allEmployees;
+    }
+
+//    @Override
+//    public void saveEmployee(Employee employee) {
+//        Session session = sessionFactory.getCurrentSession();
+//        session.saveOrUpdate(employee);
+//    }
+//
+//    @Override
+//    public Employee getEmployee(int id) {
+//        Session session = sessionFactory.getCurrentSession();
+//        Employee employee = session.get(Employee.class, id);
+//        return employee;
+//    }
+//
+//    @Override
+//    public void deleteEmployee(int id) {
+//        Session session = sessionFactory.getCurrentSession();
+//        Query<Employee> query = session.createQuery("delete from Employee " +
+//                "where id =:employeeId");
+//        query.setParameter("employeeId", id);
+//        query.executeUpdate();
+//    }
+}
